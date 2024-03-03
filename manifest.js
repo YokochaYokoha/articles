7c27470b2d3116fe34f330bdacb95bd801246bb8
横茶横葉 <108619120+YokochaYokoha@users.noreply.github.com>
2024-03-03 18:33:43 +0900

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,12 @@
+{
+  "name": "横茶横葉の記事なサイト",
+  "short_name": "記事なサイト",
+  "start_url": "/",
+  "display": "fullscreen",
+  "background_color": "#ffffff",
+  "theme_color": "#000000",
+  "icons": [{
+    "src": "./pwa.png",
+    "sizes": "144x144"
+  }]
+}
